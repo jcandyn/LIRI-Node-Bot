b7707ebbdd5d5a785bea4b997fdd9be606976453
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-07-30 15:48:43 -0400

    initial files upload

---
+++ liri.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+var input = process.argv[2]
+
+
+if (input == "concert-this") {
+
+}
+
+else if (input == "spotify-this-song") {
+
+}
+
+else if (input == "movie-this") {
+
+}
+
+else if (input == "do-what-it-says") {
+
+}
+
+
+
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: spotify.id,
+  secret: spotify.secret
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
+
