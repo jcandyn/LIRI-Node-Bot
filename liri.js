342ef2660c9b25043d7fe7857b4e9fecef33da40
Joscandy Nunez <joscandy@Joscandys-MacBook.local>
2019-07-30 20:30:38 -0400

    referenced the keys the right way

--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+console.log(keys)
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify({
+//   id: spotify.id,
+//   secret: spotify.secret
+//   });
 
 var input = process.argv[2]
 
@@ -27,9 +31,10 @@ else if (input == "do-what-it-says") {
 var Spotify = require('node-spotify-api');
  
 var spotify = new Spotify({
-  id: spotify.id,
-  secret: spotify.secret
+  id: keys.spotify.id,
+  secret: keys.spotify.secret
 });
+console.log(spotify)
  
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
